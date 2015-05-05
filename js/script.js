11709f5341c9472835dd02bd253591a96702189e
goopscoop <codybarrus@gmail.com>
2015-05-05 15:04:53 -0700

    turned off video

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(function(){
   window.sr = new scrollReveal();
 
-  $().ready(function() {
-    $('#about').tubular({videoId: 'LY_osSwlAoE'}); // where idOfYourVideo is the YouTube ID.
-  });
+  // $().ready(function() {
+  //   $('#about').tubular({videoId: 'LY_osSwlAoE'}); // where idOfYourVideo is the YouTube ID.
+  // });
 
 })
