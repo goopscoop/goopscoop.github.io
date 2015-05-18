85acb7a95a851f2eab1c832e1e68ebe4241ec435
goopscoop <codybarrus@gmail.com>
2015-05-18 06:57:44 -0700

    refined everything

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 $(function(){
   window.sr = new scrollReveal();
 
-  // $().ready(function() {
-  //   $('#about').tubular({videoId: 'LY_osSwlAoE'}); // where idOfYourVideo is the YouTube ID.
-  // });
-
 })
